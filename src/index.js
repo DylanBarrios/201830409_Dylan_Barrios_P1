43c291f918ae20d2d08e0a82f8c993af34d66eb5
dylan <dylanbarrios201830409@cunoc.edu.gt>
2019-10-08 19:56:03 -0600

    Puerto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+//imports
+const path  = require('path');
+const express = require('express');
+const app = express();
+
+//configuracion del puerto
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+app.set('port',3000);
+
+//Agregar las rutas
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+
+//escucha del puerto
+app.listen(app.get('port'), () => {
+    console.log('escuchando en el puerto', app.get('port'));
+});
